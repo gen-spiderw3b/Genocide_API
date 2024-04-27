0647fde3a6d2b9cc7e16bd28e88c707b7db1535e
spiderw3b <spiderw3b421@gmail.com>
2024-04-27 18:49:28 -0400

    Started Router

---
+++ client/src/Pages/index.js
@@ -0,0 +1,6 @@
+export { default as HomeLayout } from "./Home/HomeLayout";
+export { default as Home } from "./Home/Home";
+export { default as Register } from "./Register/Register";
+export { default as Login } from "./Login/Login";
+export { default as Error } from "./Errors/Error";
+export { default as Dashboard } from "./Dashboard/Dashboard";
