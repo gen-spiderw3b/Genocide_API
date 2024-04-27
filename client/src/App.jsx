611da5ceb3836b12fbec90abe1714a90e0ebf7dd
spiderw3b <spiderw3b421@gmail.com>
2024-04-27 01:39:47 -0400

    Create Base Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomeLayout, Home } from "./pages/index";
+
+// Browser Router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <h1>This works!</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,2 @@
+export { default as HomeLayout } from "./Home/HomeLayout";
+export { default as Home } from "./Home/Home";
