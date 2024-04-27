dc61d1b8cbba30163bb61b63a46ec1bd197cfb82
spiderw3b <spiderw3b421@gmail.com>
2024-04-26 23:38:56 -0400

    Init Genocide

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5500;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
+
+app.listen(port, () => console.log(`Server is running on ${port}`));
---
+++ client/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>This works!</h1>;
+}
+
+export default App;
