9d992a8a86b2f494c5b68a39dec794ec36437760
spiderw3b <spiderw3b421@gmail.com>
2024-04-27 19:30:30 -0400

    Error Page Done

--- client/src/Pages/Errors/Error.jsx
+++ client/src/Pages/Errors/Error.jsx
@@ -1,4 +1,27 @@
+import { useRouteError, Link } from "react-router-dom";
+import Img from "../../public/error-WMaxsmkR.jpg";
+import Wrapper from "../../Wrappers/Error/error";
+
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <img src={Img} alt="Error" className="img" />
+        <h3>Page Not Found!</h3>
+        <p>{error.data}</p>
+        <Link to="/">back to home</Link>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <div>
+        <h3>something went wrong! </h3>
+      </div>
+    </Wrapper>
+  );
 };
 export default Error;
---
+++ client/src/Wrappers/Error/error.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const Wrapper = styled.main`
+  background: red;
+  min-height: 100vh;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  img {
+    width: 70vw;
+    max-width: 600px;
+    display: block;
+    margin-bottom: 2rem;
+    margin-top: -3rem;
+  }
+  h3 {
+    margin-bottom: 0.5rem;
+  }
+  p {
+    line-height: 1.5;
+    margin-top: 0.5rem;
+    margin-bottom: 1rem;
+    color: black;
+  }
+  a {
+    text-decoration: none;
+    color: blue;
+    text-transform: capitalize;
+  }
+`;
+
+export default Wrapper;
