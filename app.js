f636a8067a8dc2863f793c0f1eb4cd77470f23f9
spiderw3b <spiderw3b421@gmail.com>
2024-05-04 03:35:14 -0400

    Add Morgan,AsyncErrors,And Connect to DataBase

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+import "express-async-errors";
 import express from "express";
 import * as dotenv from "dotenv";
+import morgan from "morgan";
+import mongoose from "mongoose";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import path from "path";
+import { log } from "console";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5500;
+
+if ((process.env.NODE_ENV = "development")) {
+  app.use(morgan("dev"));
+}
+
+//Building Front-End Progomatically
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -16,4 +26,20 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
-app.listen(port, () => console.log(`Server is running on ${port}`));
+//End Of Building Front-End Progomatically
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(
+        `Server is running on ${port},and you are connected to the database!`
+      )
+    );
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+start();
