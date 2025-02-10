67a6c91d64e105ea48fc54e5c9e79ff3cf017b5d
spiderw3b <spiderw3b421@gmail.com>
2025-02-10 03:19:07 -0500

    re testing

--- client/src/Pages/Education/Upload.jsx
+++ client/src/Pages/Education/Upload.jsx
@@ -22,19 +22,21 @@ const Upload = () => {
       const data = await customFetch.post("/education/check-course", {
         course: courseSelection,
       });
+      console.log(data);
+      console.log(data?.data?.msg);
       console.log(data.msg);
       console.log(data.status);
       //ACCEPTED STATUSCODE
       if (data.status === 202) {
         setIsCourse(false);
         setIsSection(true);
-        toast.info(data.data.msg);
+        toast.info(data.msg);
       }
       //CREATED STATUSCODE
       if (data.status === 201) {
         setIsCourse(false);
         setIsSection(true);
-        toast.success(data.data.msg);
+        toast.success(data.msg);
       }
 
       return;
