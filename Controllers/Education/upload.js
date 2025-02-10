da1b91e279556ff793c64c68ac24e3bf76bfa234
spiderw3b <spiderw3b421@gmail.com>
2025-02-10 01:59:29 -0500

    fix course and section

--- Controllers/Education/upload.js
+++ Controllers/Education/upload.js
@@ -7,7 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const checkCourse = (req, res) => {
   const { course } = req.body;
-  fs.readdir(path.join(__dirname, "../../public/upload/"), (err, files) => {
+  fs.readdir("/public", (err, files) => {
     if (err) {
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
         msg: "there was a error with routing, contact me so i can fix it!",
@@ -18,21 +18,17 @@ export const checkCourse = (req, res) => {
           .status(StatusCodes.ACCEPTED)
           .json({ msg: `${course} has already been made!` });
       } else {
-        fs.mkdir(
-          path.join(__dirname, `../../public/upload/${course}`),
-          { recursive: true },
-          (err) => {
-            if (err) {
-              res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                msg: "Error with creating course directory, contact me so i can fix this!",
-              });
-            } else {
-              res
-                .status(StatusCodes.CREATED)
-                .json({ msg: `${course} has been made!` });
-            }
+        fs.mkdir(`/public/${course}`, { recursive: true }, (err) => {
+          if (err) {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+              msg: "Error with creating course directory, contact me so i can fix this!",
+            });
+          } else {
+            res
+              .status(StatusCodes.CREATED)
+              .json({ msg: `${course} has been made!` });
           }
-        );
+        });
       }
     }
   });
@@ -40,38 +36,31 @@ export const checkCourse = (req, res) => {
 
 export const checkSection = (req, res) => {
   const { section, course } = req.body;
-  fs.readdir(
-    path.join(__dirname, `../../public/upload/${course}`),
-    (err, files) => {
-      if (err) {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-          msg: "there was a error with routing, contact me so i can fix it!",
-        });
+  fs.readdir(`/public/${course}`, (err, files) => {
+    if (err) {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        msg: "there was a error with routing, contact me so i can fix it!",
+      });
+    } else {
+      if (files.includes(`${section}`)) {
+        res
+          .status(StatusCodes.ACCEPTED)
+          .json({ msg: "section" + `${section} has already been made!` });
       } else {
-        if (files.includes(`${section}`)) {
-          res
-            .status(StatusCodes.ACCEPTED)
-            .json({ msg: "section" + `${section} has already been made!` });
-        } else {
-          fs.mkdir(
-            path.join(__dirname, `../../public/upload/${course}/${section}`),
-            { recursive: true },
-            (err) => {
-              if (err) {
-                res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                  msg: "Error with creating course directory, contact me so i can fix this!",
-                });
-              } else {
-                res
-                  .status(StatusCodes.CREATED)
-                  .json({ msg: "section" + `${section} has been made!` });
-              }
-            }
-          );
-        }
+        fs.mkdir(`/public/${course}/${section}`, { recursive: true }, (err) => {
+          if (err) {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+              msg: "Error with creating course directory, contact me so i can fix this!",
+            });
+          } else {
+            res
+              .status(StatusCodes.CREATED)
+              .json({ msg: "section" + `${section} has been made!` });
+          }
+        });
       }
     }
-  );
+  });
 };
 
 export const fileUpload = async (req, res) => {
