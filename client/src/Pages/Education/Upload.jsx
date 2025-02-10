aa608553dbdcbd40f3ec2d88b85a03ec747e2f01
spiderw3b <spiderw3b421@gmail.com>
2025-02-10 03:40:31 -0500

    test1

--- client/src/Pages/Education/Upload.jsx
+++ client/src/Pages/Education/Upload.jsx
@@ -19,12 +19,14 @@ const Upload = () => {
   const handleCourse = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await customFetch.post("/education/check-course", {
+      const data = await customFetch.post("/education/check-course", {
         course: courseSelection,
       });
 
       console.log(data);
-      console.log(data.msg);
+      console.log(data.data);
+      console.log(data.data.data);
+      console.log(data.data.data.msg);
       console.log(data.status);
 
       //ACCEPTED STATUSCODE
