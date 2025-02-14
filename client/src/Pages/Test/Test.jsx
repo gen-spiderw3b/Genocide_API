c2c863fefaf61701ee20fa11e0b9b64f8bb33c06
spiderw3b <spiderw3b421@gmail.com>
2025-02-14 08:36:15 -0500

    test5

--- client/src/Pages/Test/Test.jsx
+++ client/src/Pages/Test/Test.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 const Test = () => {
   const [src, setSrc] = useState("");
   const [isSrc, setIsSrc] = useState(false);
+  console.log(src);
+
   const handleFile = async (e) => {
     e.preventDefault();
     const formData = new FormData();
@@ -18,10 +20,11 @@ const Test = () => {
 
       //ACCEPTED STATUSCODE
       if (data.status === 202) {
-        console.log("umm");
+        console.log("already here");
       }
       //CREATED STATUSCODE
       if (data.status === 201) {
+        console.log(data);
         setSrc(data.data.file.src);
         setIsSrc(true);
       }
@@ -33,11 +36,11 @@ const Test = () => {
 
   return (
     <div>
-      <h1>test</h1>
+      <h1>tests</h1>
       <form method="POST">
         <input type="file" name="src" className="input" onChange={handleFile} />
       </form>
-      {isSrc ? <video src={src} autoPlay /> : null}
+      {isSrc ? <video src={src} autoPlay controls /> : null}
     </div>
   );
 };
