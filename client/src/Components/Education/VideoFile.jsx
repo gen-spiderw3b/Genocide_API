03f28dabf008d20b0706ec7b857211bd3c96cb1a
spiderw3b <spiderw3b421@gmail.com>
2025-02-14 07:34:24 -0500

    test2

--- client/src/Components/Education/VideoFile.jsx
+++ client/src/Components/Education/VideoFile.jsx
@@ -1,17 +1,12 @@
 import PropTypes from "prop-types";
 
 const VideoFile = ({ video }) => {
+  console.log(video);
+
   return (
     <div className="video-container">
-      <video src={video} className="video-file" controls autoPlay></video>
       <video
-        src={" /opt/render/project/src/public/math/1/section1-1.mp4"}
-        className="video-file"
-        controls
-        autoPlay
-      ></video>
-      <video
-        src={"../public/math/1/section1-1.mp4"}
+        src={"/opt/render/project/src/public/math/1/section1-1.mp4"}
         className="video-file"
         controls
         autoPlay
