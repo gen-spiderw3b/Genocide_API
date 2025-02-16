576d29fb059e545344a6b5fef829920127a7978c
spiderw3b <spiderw3b421@gmail.com>
2025-02-16 06:16:46 -0500

    fix course

--- Controllers/Education/course.js
+++ Controllers/Education/course.js
@@ -8,14 +8,6 @@ export const createFullCourse = async (req, res) => {
   const { course, section, title, src } = req.body;
   const oldCourse = await Course.findOne({ course: course });
 
-  const file = await File.create({
-    course: course,
-    section: section,
-    title: title,
-    src: src,
-    createdBy: req.user.userId,
-  });
-
   if (oldCourse === null) {
     const file = await File.create({
       course: course,
